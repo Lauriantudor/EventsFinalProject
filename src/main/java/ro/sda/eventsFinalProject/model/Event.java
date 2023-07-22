547660080535875935b653dd2f70b9fdfc9d61c7
Lauriantudor <52708705+Lauriantudor@users.noreply.github.com>
2023-07-22 19:02:44 +0300

    Table events created

---
+++ src/main/java/ro/sda/eventsFinalProject/model/Event.java
@@ -0,0 +1,30 @@
+package ro.sda.eventsFinalProject.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "events")
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    @Column(nullable = false)
+    private String name;
+    @Column(name = "start_date")
+    private LocalDateTime startDate;
+    @Column(name = "end_date")
+    private LocalDateTime endDate;
+    private String description;
+    private String location;
+
+}
